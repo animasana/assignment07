e1213efd8db1cd50ffb2c78c5d710b81affc6b04
Oogway <animagig@gmail.com>
2026-01-04 19:35:39 +0900

    Retry Sidebar

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langchain_core.prompts.prompt import PromptTemplate
 
 st.title("Assignment07")
 
-
 function = {
     "name": "create_quiz",
     "description": "function that takes a list of questions and answers and returns a quiz",
@@ -51,6 +50,8 @@ with st.sidebar:
     OPENAI_API_KEY = st.text_input(label="OPENAI API KEY")
     difficulty = st.radio("Degree of Difficulty", ["Easy", "Hard"], index=0)    
     topic = st.text_input("Input a topic about quiz you want to create")
+    if st.button("Retry"):
+        st.rerun()
     st.write("https://github.com/animasana/assignment07/blob/main/app.py")
 
 
@@ -98,7 +99,7 @@ else:
     response = run_quiz_chain(topic=topic, difficulty=difficulty)
     response = json.loads(response.additional_kwargs["function_call"]["arguments"])
 
-    with st.form("questions_form", clear_on_submit=True, enter_to_submit=False):
+    with st.form("questions_form", clear_on_submit=True):
         correct_answers = 0
         for i, question in enumerate(response["questions"]):
             st.write(question["question"])            
@@ -115,18 +116,16 @@ else:
                 elif value is not None:
                     st.error("Wrong")        
         
-        is_perfect = len(response["questions"]) == correct_answers
-
-        submitted = st.form_submit_button("Submit", disabled=is_perfect)        
+        submitted = st.form_submit_button("Submit")
         
-        if submitted:
-            if is_perfect:            
-                st.balloons()            
-                st.success("Perfect score!")                
-            else:            
-                st.error("Not perfect, Try again?")                
-                if st.form_submit_button("Retry"):                    
-                    st.rerun()
+    if submitted:            
+        if len(response["questions"]) == correct_answers:
+            st.balloons()
+            st.success("Perfect score!")            
+        else:
+            st.error("Not perfect, Try again?")    
+            
+                
                     
         
     
